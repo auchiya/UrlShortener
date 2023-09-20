0122f9ee8b31279e5c2b74201311e232d6093008
aUchiya <Agustin.Uchiya@derivco.co.uk>
2023-09-20 11:35:19 +0200

    Add project files.

---
+++ UrlShortener/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using UrlShortener.Entities;
+using UrlShortener.Services;
+
+namespace UrlShortener;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options)
+        : base(options)
+    {
+    }
+
+    public DbSet<ShortenedUrl> ShortenedUrls { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ShortenedUrl>(builder =>
+        {
+            builder.Property(s => s.Code).HasMaxLength(UrlShorteningService.NumberOfCharsInShortLink);
+
+            builder.HasIndex(s => s.Code).IsUnique();
+        });
+    }
+}
---
+++ UrlShortener/Extensions/MigrationExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UrlShortener.Extensions;
+
+public static class MigrationExtensions
+{
+    public static WebApplication ApplyMigrations(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+
+        var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        dbContext.Database.Migrate();
+
+        return app;
+    }
+}
